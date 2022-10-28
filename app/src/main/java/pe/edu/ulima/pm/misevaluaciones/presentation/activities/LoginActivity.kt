407d143c9d041721d048916c34939ef76d3f8943
ProfL3403 <ProfL3403@lab.aca>
2022-10-28 10:08:14 -0500

    Shared preferences terminado

--- app/src/main/java/pe/edu/ulima/pm/misevaluaciones/presentation/activities/LoginActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/misevaluaciones/presentation/activities/LoginActivity.kt
@@ -1,16 +1,25 @@
 package pe.edu.ulima.pm.misevaluaciones.presentation.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import pe.edu.ulima.pm.misevaluaciones.MainActivity
 import pe.edu.ulima.pm.misevaluaciones.presentation.screens.login.LoginScreen
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
-            LoginScreen(onLoginSuccess = guardarCredencialesLogin)
+        // Consultar el SharedPreferences
+        val username = obtenerCredencialesLogin()
+        if (username != null && username != "") {
+            // No es la primera vez (ya me loguie antes)
+            pasarAlMain()
+        }else {
+            setContent {
+                LoginScreen(onLoginSuccess = guardarCredencialesLogin)
+            }
         }
     }
 
@@ -18,5 +27,19 @@ class LoginActivity : ComponentActivity() {
         // Guardarlo en shared preferences
         val sp = getSharedPreferences("SP_CREDENCIALES", MODE_PRIVATE)
         sp.edit().putString("USERNAME", username).commit()
+
+        pasarAlMain()
+    }
+
+    private fun pasarAlMain() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    val obtenerCredencialesLogin : () -> String? = {
+        val sp = getSharedPreferences("SP_CREDENCIALES", MODE_PRIVATE)
+        val username = sp.getString("USERNAME", "")
+        username
     }
 }
